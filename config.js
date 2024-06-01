8c5b7bc8e004f9f645044ea6bfad63f2d226f6d2
MonovanShop <164271382+MonovanShop@users.noreply.github.com>
2024-06-01 16:52:48 -0600

    Update config.js

--- config.js
+++ config.js
@@ -11,13 +11,13 @@ import { en, es, id, ar, pt, fr, hi } from "./lib/idiomas/total-idiomas.js"
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a | Add the numbers to be Owner ⋅.━━━━ ⊰  
 
 global.owner = [
-["5215625775955", 'Monovan', true], //Richetti
-["5215625775955"], 
-["5215625775955"], 
-["5215625775955"], 
-["5215625775955"],
-["5217298724948"],
-["5215625768269"]]
+["5215610035034", 'oreo bot', true], //oreo bot
+["5215610035034"], 
+["5215610035034"], 
+["5215610035034"], 
+["5215610035034"],
+["5215610035034"],
+["5215610035034"]]
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
@@ -26,8 +26,8 @@ global.owner = [
 global.botNumberCode = "" //Ejemplo: +59309090909
 global.confirmCode = ""
 
-global.mods = [5215625775955]
-global.prems = ["5215625775955"]
+global.mods = [5215610035034]
+global.prems = ["5215610035034"]
 //key de violetics
 global.Key360 = ["964f-0c75-7afc"]
    
@@ -103,47 +103,47 @@ global.moment = moment
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.official = [ // Agregate si eres Owner
-["5215625775955", 'MonkeyBot', 1], 
-["5215625775955", 'MonkeyBot', 1],  
-["5215625775955", 'MonkeyBot', 1],
-["5217298724948", 'MonkeyBot', 1],
-["5215625768269", 'MonkeyBot', 1]]
+["5215610035034", 'oreo bot', 1], 
+["5215610035034", 'oreo bot', 1],  
+["5215610035034", 'oreo bot', 1],
+["5215610035034", 'oreo bot', 1],
+["5215610035034", 'oreo bot', 1]]
 
 global.mail = '' //agrega tú correo
 global.desc = 'MonkeyBot el mejor bot de whatsapp' //agrega una descripción corta
 global.desc2 = 'MonkeyBot el mejor bot de whatsapp' //agrega una descripción larga (Solo se aplicará si su whasapp no tiene descripción)
 global.country = 'Perú' //agrega tú país ejemplo: 🇪🇨
 
-global.packname = "MonkeyBot"
-global.author = "MonkeyBot"
+global.packname = "oreo bot"
+global.author = "oreo bot"
 
 //⊱ ━━━━━.⋅ Versión | Nombre | cuentas ⋅.━━━━ ⊰
 
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
 
-global.gt = "MonkeyBot"
-global.yt = "https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb"
-global.yt2 = "https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb"
-global.ig = "https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb"
-global.md = "https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb"
-global.fb = "https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb"
+global.gt = "oreo bot"
+global.yt = "https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K"
+global.yt2 = "https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K"
+global.ig = "https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K"
+global.md = "https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K"
+global.fb = "https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K"
 
-global.nna = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //CANAL UPDATE
-global.nn2 = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //UPDATE
-global.nna2 = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Help
-global.nn = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 1
-global.nnn = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 2
-global.nnnt = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 3
-global.nnntt = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 4
-global.nnnttt = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 5
-global.nnnttt1 = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb'; //Grupo 6 COL
-global.nnnttt2 = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 7 COL
-global.nnnttt3 = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 8 COL
-global.nnnttt4 = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //Grupo 9 COL
-global.nnnttt5 = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb' //A.T.M.M
-global.paypal = 'https://chat.whatsapp.com/Ig0BhML7t4G8vmnmPrCKNb'
-global.asistencia = 'Wa.me/525625768269' //Dudas? escríbeme...
+global.nna = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //CANAL UPDATE
+global.nn2 = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //UPDATE
+global.nna2 = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Help
+global.nn = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 1
+global.nnn = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 2
+global.nnnt = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 3
+global.nnntt = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 4
+global.nnnttt = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 5
+global.nnnttt1 = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K'; //Grupo 6 COL
+global.nnnttt2 = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 7 COL
+global.nnnttt3 = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 8 COL
+global.nnnttt4 = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //Grupo 9 COL
+global.nnnttt5 = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K' //A.T.M.M
+global.paypal = 'https://chat.whatsapp.com/FnTTAJ8mzO3EAlR9R8g74K'
+global.asistencia = 'Wa.me/525610035034' //Dudas? escríbeme...
 
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ ฅ^•ﻌ•^ฅ
 
@@ -177,8 +177,8 @@ global.exito = eg
 
 //⊱ ━━━━━.⋅ Información | Information ⋅.━━━━ ⊰
 
-global.wm = "MonkeyBot"
-global.igfg = "MonkeyBot"
+global.wm = "oreo bot"
+global.igfg = "oreo bot"
 global.wait = "*⌛ _Cargando | Charging..._ ▬▭▭▭▭▭▭*"
 global.waitt = "*⌛ _Cargando | Charging..._ ▬▬▭▭▭*"
 global.waittt = "*⌛ _Cargando | Charging..._ ▬▬▬▬▭▭*"
