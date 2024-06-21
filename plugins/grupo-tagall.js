16e5ec9ae7e03bb058b80890f4e0e89da9d9482d
Antonyxit <carlosmorga45@gmail.com>
2024-06-21 00:32:06 -0600

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -6,10 +6,10 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
 let oi = `*🌤️@𝖈𝖊𝖔𝖌𝖊𝖗𝖎𝖕𝖎𝖚𝖒* ${pesan}`
-let teks = `*😸MonkeyBot 𝙩𝙚 𝙞𝙣𝙫𝙤𝙘𝙖 𝙥𝙡𝙖𝙣𝙩𝙖😼*\n${oi}\n\n*✨𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`
+let teks = `*🌸𝘼𝙉𝙄𝙈𝙊 𝙋𝙍𝙀𝘾𝙄𝙊𝙎@𝙎 𝙏𝘼𝙉𝙄 𝘽𝙊𝙏 𝙏𝙀 𝙄𝙉𝙑𝙊𝘾𝘼🌸*\n${oi}\n\n*✨𝙈𝙚𝙣𝙘𝙞𝙤𝙣𝙚𝙨*\n`
 for (let mem of participants) {
-teks += `☀️⛱️ @${mem.id.split('@')[0]}\n`}
-teks += `MonkeyBot`
+teks += `🌸 @${mem.id.split('@')[0]}\n`}
+teks += `𝙏𝘼𝙉𝙄 𝘽𝙊𝙏`
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
